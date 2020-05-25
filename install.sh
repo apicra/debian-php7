306d9edf22c43ea79e9300187d8079d270150f49
tom <tom@sapletta.com>
2020-05-25 14:11:08 +0200

    php environment

--- install.sh
+++ install.sh
@@ -6,8 +6,7 @@ apt-get update
 #echo "deb https://packages.sury.org/php/ $(lsb_release -sc) main" | tee /etc/apt/sources.list.d/php.list
 sudo apt-get -y install lsb-release apt-transport-https ca-certificates
 sudo wget -O /etc/apt/trusted.gpg.d/php.gpg https://packages.sury.org/php/apt.gpg
-echo “deb https://packages.sury.org/php/ $(lsb_release -sc) main” | sudo tee /etc/apt/sources.list.d/php7.3.list
-
+echo "deb https://packages.sury.org/php/ $(lsb_release -sc) main" | sudo tee /etc/apt/sources.list.d/php7.3.list
 apt-get update
 apt-get install php7.3
 php -v
