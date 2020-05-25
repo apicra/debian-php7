600c549dbecdb932d00d696f0e8d571c64eb47c8
tom <tom@sapletta.com>
2020-05-25 14:25:22 +0200

    php environment

--- install.sh
+++ install.sh
@@ -4,9 +4,10 @@ apt-get update
 #apt-get install ca-certificates apt-transport-https
 #wget -q https://packages.sury.org/php/apt.gpg -O- | apt-key add -
 #echo "deb https://packages.sury.org/php/ $(lsb_release -sc) main" | tee /etc/apt/sources.list.d/php.list
-sudo apt-get install ca-certificates apt-transport-https
+apt-get install ca-certificates apt-transport-https
 wget -q https://packages.sury.org/php/apt.gpg -O- | sudo apt-key add -
 echo "deb https://packages.sury.org/php/ stretch main" | sudo tee /etc/apt/sources.list.d/php.list
 apt-get update
-apt-get install php7.3
+apt-get install php7.4
+apt-get install php7.4-cli php7.4-common php7.4-curl php7.4-mbstring php7.4-mysql php7.4-xml
 php -v
